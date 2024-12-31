608e9146573052e008b3b8682c96d89ec1213df3
Vancouvernoouchi <vancouver.no.ouchi@gmail.com>
2024-12-31 10:57:20 +0900

    feature: snsの外部リンクは別タブで開く設定

--- frontend/config/headerOptions.ts
+++ frontend/config/headerOptions.ts
@@ -4,40 +4,49 @@ import { JP, CA, CN, FlagComponent } from "country-flag-icons/react/3x2";
 interface HeaderOptionsProps {
   name: string;
   href: string;
+  isExternalLink: boolean; // 外部リンクかどうか
 }
 
 export const headerOptions: HeaderOptionsProps[] = [
   {
     name: "ホーム",
     href: "/",
+    isExternalLink: false,
   },
   {
     name: "シェアハウスを探す",
     href: "/properties",
+    isExternalLink: false,
   },
   {
     name: "生活ガイド",
     href: LINKS.GUIDE,
+    isExternalLink: true,
   },
   {
     name: "英語レッスン",
     href: LINKS.THREADS,
+    isExternalLink: true,
   },
   {
     name: "会社情報",
     href: "/company",
+    isExternalLink: false,
   },
   {
     name: "お問い合わせ",
     href: LINKS.INSTAGRAM,
+    isExternalLink: true,
   },
   {
     name: "家を探されている方",
     href: "/tenant",
+    isExternalLink: false,
   },
   {
     name: "物件掲載希望の方",
     href: "/landlord",
+    isExternalLink: false,
   },
 ];
 
