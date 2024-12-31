3242ba716605ca92b53ee6d9154d9c77ecff2b83
Vancouvernoouchi <vancouver.no.ouchi@gmail.com>
2024-12-31 11:23:56 +0900

    add : 他言語ページのルーティングを追加

---
+++ frontend/app/(main)/chinese/page.tsx
@@ -0,0 +1,12 @@
+import DevelopingPage from "@/components/atoms/common/DevelopingPage";
+import React from "react";
+
+const ChinesePage = () => {
+  return (
+    <main>
+      <DevelopingPage />
+    </main>
+  );
+};
+
+export default ChinesePage;
---
+++ frontend/app/(main)/english/page.tsx
@@ -0,0 +1,12 @@
+import DevelopingPage from "@/components/atoms/common/DevelopingPage";
+import React from "react";
+
+const EnglishPage = () => {
+  return (
+    <main>
+      <DevelopingPage />
+    </main>
+  );
+};
+
+export default EnglishPage;
