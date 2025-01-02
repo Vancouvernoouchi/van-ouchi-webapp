6e31bbaafb24c9fe469a679cecc08503b8c1c5e1
Manami Batai <vaattii1996@gmail.com>
2025-01-02 15:39:57 +0900

    fix: 詳細画面のproperties/property/idのパスを/properties/idへ修正

--- frontend/app/(main)/properties/[propertyId]/error.tsx
+++ frontend/app/(main)/properties/[propertyId]/error.tsx
@@ -1,9 +1,10 @@
 "use client";
+
 import ErrorPage from "@/components/atoms/common/ErrorPage";
 import { MESSAGES } from "@/constants/messages";
 
-const PropertiesListError = () => {
+const PropertyDetailPageError = () => {
   return <ErrorPage errorMessage={MESSAGES.ERROR_UNEXPECTED} />;
 };
 
-export default PropertiesListError;
+export default PropertyDetailPageError;
--- frontend/app/(main)/properties/property/[propertyId]/error.tsx
+++ frontend/app/(main)/properties/property/[propertyId]/error.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-const PropertyPageError = () => {
-  return (
-    <div className="h-[88vh] p-2 flex flex-col justify-center items-center text-center text-red-500 text-xl">
-      予期せぬエラーが発生しました。もう一度お試しください。
-    </div>
-  );
+import ErrorPage from "@/components/atoms/common/ErrorPage";
+import { MESSAGES } from "@/constants/messages";
+
+const PreviousPropertyDetalPageError = () => {
+  return <ErrorPage errorMessage={MESSAGES.ERROR_UNEXPECTED} />;
 };
 
-export default PropertyPageError;
+export default PreviousPropertyDetalPageError;
