2e1d12f5dc6b328fded76ebe568088ae4aab606e
yusuken1121 <yusuken1121@gmail.com>
2025-01-15 15:38:14 -0800

    ✨ ソート機能の追加。properties => roomsに名称と取得項目を変更。

--- backend/src/controllers/testController.ts
+++ backend/src/controllers/testController.ts
@@ -4,13 +4,50 @@ import prisma from "../lib/prisma";
 import { Prisma } from "@prisma/client";
 import { handlePrismaError } from "../lib/errorHandler";
 
-export const getAllProperties = async (req: Request, res: Response) => {
+/**TODO
+ * 1. Done: ルームを全て取得する
+ * 2. ページネーション、フィルタリング、ソートを実装する
+ */
+export const getAllRooms = async (req: Request, res: Response) => {
   try {
-    /**TODO
-     * 1. Done: プロパティを全て取得する
-     * 2. Done: ページネーション、フィルタリング、ソートを実装する
+    const { page = 1, itemsPerPage = 20, sortBy = "latest" } = req.query;
+
+    /**
+     * ソート DONE
+     * 1. 金額の昇順
+     * 2. 金額の降順
+     * 3. 新着順
      */
-    const allProperties = await prisma.property.findMany();
+    let orderBy: Prisma.RoomOrderByWithRelationInput;
+    switch (sortBy) {
+      case "price-asc":
+        orderBy = { rent: "asc" };
+        break;
+      case "price-desc":
+        orderBy = { rent: "desc" };
+        break;
+      case "latest":
+        orderBy = { createdAt: "asc" };
+        break;
+      default:
+        orderBy = { createdAt: "asc" };
+        break;
+    }
+
+    /**
+     * フィルタリング
+     * 1. 認証済みの物件のみ
+     * 2. その他の条件
+     */
+
+    // ページネーション
+
+    // roomsテーブルから全てのプロパティを取得
+    const allProperties = await prisma.room.findMany({
+      orderBy,
+      take: Number(itemsPerPage),
+      include: { property: true },
+    });
     if (allProperties.length === 0) {
       res.status(404).json({ message: ERROR_MESSAGE.API.INVALID });
       return;
@@ -21,7 +58,7 @@ export const getAllProperties = async (req: Request, res: Response) => {
   }
 };
 
-export const getPropertyById = async (req: Request, res: Response) => {
+export const getRoomById = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const property = await prisma.property.findUnique({
--- backend/src/routes/testRouter.ts
+++ backend/src/routes/testRouter.ts
@@ -1,10 +1,7 @@
 import { Router } from "express";
-import {
-  getAllProperties,
-  getPropertyById,
-} from "../controllers/testController";
+import { getAllRooms, getRoomById } from "../controllers/testController";
 
 export const router = Router();
 
-router.get("/", getAllProperties);
-router.get("/:id", getPropertyById);
+router.get("/", getAllRooms);
+router.get("/:id", getRoomById);
