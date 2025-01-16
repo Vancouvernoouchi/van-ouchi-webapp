5799a4b69f0ea316bc703bc81731645959991c37
Manami Batai <vaattii1996@gmail.com>
2025-01-17 00:13:34 +0900

    modify: 物件詳細画面の地図を非表示、インスタの埋め込みリンク変更

---
+++ frontend/components/atoms/common/MapNotFound.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+/**
+ * 地図が表示できない時のコンポーネント
+ *
+ * @param message {string}
+ * @param className {string}
+ */
+export const MapNotFound = ({
+  message,
+  className,
+}: {
+  message: string;
+  className?: string;
+}) => {
+  return (
+    <div className="relative flex items-center justify-center w-full h-full">
+      {/* 地図画像 */}
+      <Image
+        src="/vancouver_map.png"
+        alt="バンクーバーの地図"
+        width={400}
+        height={300}
+        objectFit="cover"
+        className={cn("w-full h-[400px] object-cover opacity-70", className)}
+      />
+      {/* エラーメッセージ */}
+      <div className="absolute text-center text-white p-4 bg-black/60 rounded-md text-sm sm:text-base">
+        <p>{message}</p>
+        <p>詳しい住所はスタッフまでお問い合わせください。</p>
+      </div>
+    </div>
+  );
+};
