8f5d739403e838683db508017a26de5647c3a2b9
Manami Batai <vaattii1996@gmail.com>
2025-02-02 20:39:55 +0900

    fix: 依存関係を更新

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,12 +1,19 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://www.van-ouchi.com"], // 許可するフロントエンドのURL
+      methods: ["GET", "POST", "PUT", "DELETE"], // 許可するHTTPメソッド
+      allowedHeaders: ["Content-Type", "Authorization"], // 許可するヘッダー
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
