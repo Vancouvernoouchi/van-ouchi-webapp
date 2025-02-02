570ec1f7350760e4b81aaa265ab21ce8b80e611c
Manami Batai <vaattii1996@gmail.com>
2025-02-02 15:00:52 +0900

    feature: 開発中パスの追加

---
+++ frontend/app/(main)/developing/page.tsx
@@ -0,0 +1,11 @@
+import { DevelopingPage } from "@/components/common";
+
+const Developing = () => {
+  return (
+    <main>
+      <DevelopingPage />
+    </main>
+  );
+};
+
+export default Developing;
