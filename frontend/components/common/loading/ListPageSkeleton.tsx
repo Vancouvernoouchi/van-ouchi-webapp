86369e5fdedc8d176d05f6528a2056c7afdc637f
Manami Batai <vaattii1996@gmail.com>
2025-02-15 12:25:36 +0900

    fix: 一覧画面のスケルトン修正

--- frontend/components/common/loading/ListPageSkeleton.tsx
+++ frontend/components/common/loading/ListPageSkeleton.tsx
@@ -6,23 +6,20 @@ import { Skeleton } from "../../ui/skeleton";
  */
 function ListPageSkeleton() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4 lg:gap-6 base-px py-10">
       {[...Array(30)].map((_, i) => (
-        <div
-          key={i}
-          className="relative shadow-md border border-gray-100 rounded-lg"
-        >
+        <div key={i} className="relative rounded-lg">
           {/* 画像エリア */}
-          <div className="z-0 min-w-full h-64 sm:h-48 xl:h-56 bg-slate-300 relative">
+          <div className="w-full rounded-lg aspect-[9/8] bg-slate-300 relative">
             <Skeleton className="w-full h-full" />
           </div>
           {/* 詳細エリア */}
           <div className="flex flex-col gap-1 w-full h-full p-2 pb-5">
             <div className="flex items-center">
-              <Skeleton className="w-32 h-4 rounded" />
+              <Skeleton className="w-full h-4 rounded" />
             </div>
             <div className="flex items-center">
-              <Skeleton className="w-20 h-4 rounded" />
+              <Skeleton className="w-40 h-4 rounded" />
             </div>
             <div className="flex items-center">
               <Skeleton className="w-24 h-4 rounded" />
