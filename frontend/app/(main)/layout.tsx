5a25d6c85510f908cd1653775281e99b409bd552
Manami Batai <vaattii1996@gmail.com>
2025-02-15 12:27:12 +0900

    add: ページ全体のローディングを追加

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { Header } from "@/components/common/header";
 import { Categories } from "@/components/common";
 import { Footer } from "@/components/common/footer";
+import { Loader } from "@/components/common/loading";
 
 const MainLayout = ({
   children,
@@ -14,8 +15,7 @@ const MainLayout = ({
       <Header />
       <main className="min-h-[calc(100vh-64px-64px)] lg:min-h-[calc(100vh-80px-68px)] z-0">
         <Categories />
-        {/* TODO: fallback */}
-        <Suspense>{children}</Suspense>
+        <Suspense fallback={<Loader />}>{children}</Suspense>
       </main>
       <Analytics />
       <Footer />
