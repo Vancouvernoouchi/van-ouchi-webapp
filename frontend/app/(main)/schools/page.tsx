629c7f770e6bb88351b2fdf4c86f36db8de73e14
Manami Batai <vaattii1996@gmail.com>
2025-02-24 19:03:06 +0900

    fix: カテゴリーヘッダーの名前の見直し

---
+++ frontend/app/(main)/schools/page.tsx
@@ -0,0 +1,11 @@
+import { DevelopingPage } from "@/components/common/page";
+
+const SchoolPage = () => {
+  return (
+    <main>
+      <DevelopingPage />
+    </main>
+  );
+};
+
+export default SchoolPage;
