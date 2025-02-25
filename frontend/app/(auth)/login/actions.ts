2e56ecc3fff971db4f697ba19b96a3338f5c574f
yusuken1121 <yusuken1121@gmail.com>
2025-02-24 17:27:42 -0800

    ⚡️ テスト用にinput項目を追加

--- frontend/app/(auth)/login/actions.ts
+++ frontend/app/(auth)/login/actions.ts
@@ -32,6 +32,12 @@ export async function signup(formData: FormData) {
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
+    options: {
+      data: {
+        name: formData.get("name") as string,
+        email: formData.get("email") as string,
+      },
+    },
   };
 
   const { error } = await supabase.auth.signUp(data);
--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -7,6 +7,8 @@ export default function LoginPage() {
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" type="password" required />
+      <label htmlFor="name">Name:</label>
+      <input id="name" name="name" type="text" />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
